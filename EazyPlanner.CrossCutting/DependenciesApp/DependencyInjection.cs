55b58024820ca2e25054edc9b79cd3fd83c30e14
Emersonmrbr <54491594+Emersonmrbr@users.noreply.github.com>
2024-02-08 21:23:28 -0300

    Start presentetion

---
+++ EazyPlanner.CrossCutting/DependenciesApp/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using EazyPlanner.Domain.Abstractions;
+using EazyPlanner.Infrastructure.Context;
+using EazyPlanner.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace EazyPlanner.CrossCutting.DependenciesApp
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+     IConfiguration configuration)
+        {
+            var connectionString = configuration
+                                  .GetConnectionString("PostgresString") ?? throw new InvalidOperationException("Connection string 'SystemConnection' not found.");
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+            services.AddScoped<ICustomerSupplierRepository, CustomerSupplierRepository>();
+            //services.AddScoped<ILivroService, LivroService>();
+
+            return services;
+        }
+    }
+}
--- EazyPlanner.Presentation/Program.cs
+++ EazyPlanner.Presentation/Program.cs
@@ -1,10 +1,15 @@
 using EazyPlanner.Presentation.Components;
+using EazyPlanner.CrossCutting.DependenciesApp;
+using EazyPlanner.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -14,6 +19,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var applicationContext = services.GetRequiredService<ApplicationDbContext>();
+    applicationContext.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
